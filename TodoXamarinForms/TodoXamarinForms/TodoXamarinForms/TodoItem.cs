fface3c1974e4a64af728bc225e23f5e01ceeda8
Matt Hoffman <hofmadresu@gmail.com>
2018-04-18 17:12:59 -0400

    View Model setup + post text

---
+++ TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoXamarinForms
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; } 
+    }
+}
---
+++ TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/TodoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmHelpers;
+using System.Collections.ObjectModel;
+
+namespace TodoXamarinForms
+{
+    public class TodoListViewModel : BaseViewModel
+    {
+        public ObservableCollection<TodoItem> TodoList { get; set; } = new ObservableCollection<TodoItem>
+        {
+            new TodoItem { Id = 0, Title = "Create First Todo", IsCompleted = true},
+            new TodoItem { Id = 1, Title = "Run a Marathon"},
+            new TodoItem { Id = 2, Title = "Create TodoXamarinForms blog post"},
+        };
+    }
+}
