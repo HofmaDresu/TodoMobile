8e34194ac9071816522ddf7a7bf624957008f80b
Matt Hoffman <hofmadresu@gmail.com>
2018-04-19 14:56:05 -0400

    use abstract class for betterness

--- TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/BaseFodyObservable.cs
+++ TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/BaseFodyObservable.cs
@@ -2,7 +2,7 @@
 
 namespace TodoXamarinForms
 {
-    class BaseFodyObservable : INotifyPropertyChanged
+    abstract class BaseFodyObservable : INotifyPropertyChanged
     {
 #pragma warning disable CS0067
         public event PropertyChangedEventHandler PropertyChanged;
