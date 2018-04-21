8a9d53ed0a9645c85ba24b10f3c4b6013f536585
Matt Hoffman <hofmadresu@gmail.com>
2018-04-21 11:35:08 -0400

    Converter for toggling IsCompleted

---
+++ TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/ChangeCompleteActionTextConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TodoXamarinForms
+{
+    public class ChangeCompleteActionTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isCompleted = (bool)value;
+            return isCompleted ? "Uncomplete" : "Complete";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Not used since we only want to convert a boolean to text, and not the other way around
+            throw new NotImplementedException();
+        }
+    }
+}
