14bb9ebd6ba762b83e27fddd8fc9264495eef13d
Matt Hoffman <hofmadresu@gmail.com>
2018-04-20 21:26:31 -0400

    Group list

--- TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/TodoListViewModel.cs
+++ TodoXamarinForms/TodoXamarinForms/TodoXamarinForms/TodoListViewModel.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TodoXamarinForms
 {
     class TodoListViewModel : BaseFodyObservable
     {
+        public TodoListViewModel()
+        {
+            GroupedTodoList = GetGroupedTodoList();
+        }
+
+        public ILookup<string, TodoItem> GroupedTodoList { get; set; }
         public string Title => "My Todo list";
-        public ObservableCollection<TodoItem> TodoList { get; set; } = new ObservableCollection<TodoItem>
+
+        private List<TodoItem> _todoList = new List<TodoItem>
         {
             new TodoItem { Id = 0, Title = "Create First Todo", IsCompleted = true},
             new TodoItem { Id = 1, Title = "Run a Marathon"},
             new TodoItem { Id = 2, Title = "Create TodoXamarinForms blog post"},
         };
+
+        private ILookup<string, TodoItem> GetGroupedTodoList()
+        {
+            return _todoList.OrderBy(t => t.IsCompleted).ToLookup(t => t.IsCompleted? "Completed" : "Active");
+        }
     }
 }
