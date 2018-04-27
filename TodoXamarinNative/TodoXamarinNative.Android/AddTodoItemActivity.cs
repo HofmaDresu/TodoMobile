4d61f4f6d4c249a2b06c133aae7ee07384ca26a6
Matt Hoffman <hofmadresu@gmail.com>
2018-04-27 12:15:41 -0400

    new screen axml and activity

---
+++ TodoXamarinNative/TodoXamarinNative.Android/AddTodoItemActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace TodoXamarinNative.Android
+{
+    [Activity(Label = "AddTodoItemActivity")]
+    public class AddTodoItemActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.AddTodoItem);
+        }
+    }
+}
