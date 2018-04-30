6ba7f138566b36a783d696ce74e809ac50acaaa6
Matt Hoffman <hofmadresu@gmail.com>
2018-04-30 12:29:08 -0400

    Display initial list

--- TodoXamarinNative/TodoXamarinNative.iOS/MainViewController.cs
+++ TodoXamarinNative/TodoXamarinNative.iOS/MainViewController.cs
@@ -4,10 +4,37 @@ namespace TodoXamarinNative.iOS
 {
     class MainViewController : UIViewController
     {
+        private UITableView _todoTableView;
+
         public MainViewController()
         {
             Title = "Todo List";
             View.BackgroundColor = UIColor.White;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _todoTableView = new UITableView
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false
+            };
+            View.Add(_todoTableView);
+
+            _todoTableView.TopAnchor.ConstraintEqualTo(View.TopAnchor).Active = true;
+            _todoTableView.BottomAnchor.ConstraintEqualTo(View.BottomAnchor).Active = true;
+            _todoTableView.LeftAnchor.ConstraintEqualTo(View.LeftAnchor).Active = true;
+            _todoTableView.RightAnchor.ConstraintEqualTo(View.RightAnchor).Active = true;
+        }
+
+        public override async void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            var todoList = await AppDelegate.TodoRepository.GetList();
+            _todoTableView.Source = new TodoItemTableSource(todoList);
+            _todoTableView.ReloadData();
         }
     }
 }
---
+++ TodoXamarinNative/TodoXamarinNative.iOS/TodoItemTableSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using TodoXamarinNative.Core;
+using UIKit;
+
+namespace TodoXamarinNative.iOS
+{
+    class TodoItemTableSource : UITableViewSource
+    {
+        private const string CellIdentifier = "TodoItemCell";
+        private readonly List<TodoItem> _todoItems;
+
+        public TodoItemTableSource(List<TodoItem> todoItems)
+        {
+            _todoItems = todoItems;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+            cell.TextLabel.Text = _todoItems[indexPath.Row].Title;
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return _todoItems.Count;
+        }
+    }
+}
