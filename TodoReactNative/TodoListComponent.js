453723644035dd1d7d994693ca6bfa0f319b4fb3
Matt Hoffman <hofmadresu@gmail.com>
2018-06-01 10:41:30 -0400

    Show sections

--- TodoReactNative/TodoListComponent.js
+++ TodoReactNative/TodoListComponent.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, SectionList, Text } from 'react-native';
 import TodoItemComponent from './TodoItemComponent';
 
 
 
 export default function TodoList({todoItems, ...props}) {
+  let activeItems = todoItems.filter(i => !i.isCompleted);
+  let completedItems = todoItems.filter(i => i.isCompleted);
+  let sections = [
+    { title:"Active", data:activeItems },
+    { title:"Completed", data:completedItems},
+  ];
+
   return (
-    <FlatList style={styles.container}
-      data={todoItems}
+    <SectionList style={styles.container}
+      sections={sections}
       renderItem={({item, index, section}) => <TodoItemComponent {...item} />}
+      renderSectionHeader={({section: {title}}) => (
+        <Text style={styles.sectionHeader}>{title}</Text>
+      )}
     />
   );
 }
@@ -17,5 +27,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  sectionHeader: {
+    fontWeight: 'bold',
+    backgroundColor: '#eee',
+    paddingTop: 5,
+    paddingBottom: 5,    
   },
 });
